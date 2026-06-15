4525f184410eb95e7084dffa1fb81ac2a0cccc5f
tsasaki-lab <293791898+tsasaki-lab@users.noreply.github.com>
2026-06-15 19:52:25 +0900

    Add search page

---
+++ src/main/java/com/tsasaki/marketfinder/controller/SearchController.java
@@ -0,0 +1,17 @@
+package com.tsasaki.marketfinder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+
+    @GetMapping("/search")
+    public String search(String keyword, Model model) {
+
+        model.addAttribute("keyword", keyword);
+
+        return "search";
+    }
+}
